bbe4cbeb5a320bab9562f43dcfcbf5acd4a584df
K-onrad <g.konrad@outlook.com>
2020-04-10 10:49:15 -0300

    swapping VUE for svelte

---
+++ {{cookiecutter.project_slug}}/frontend/src/components/Sidebar.svelte
@@ -0,0 +1,12 @@
+<style>
+  .sidenav {
+    position: static;
+    width: 17.85714rem;
+    background-color: #2e3c56;
+    color: #d4dade;
+    z-index: auto;
+  }
+</style>
+
+<div class='sidenav'>
+</div>
---
+++ {{cookiecutter.project_slug}}/frontend/src/components/Topbar.svelte
@@ -0,0 +1,57 @@
+<script>
+  export let segment
+</script>
+
+<style>
+  nav {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: auto;
+    z-index: 101;
+    width: 100%;
+    background-color: #222e44;
+    color: #fff;
+  }
+  ul {
+    display: flex;
+    min-height: 4.64286rem;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  /* clearfix */
+  ul::after {
+    content: '';
+    clear: both;
+  }
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  a {
+    font-size: 0.92857rem;
+    text-decoration: none;
+  }
+  .logo {
+    width: 250px;
+    background-color: #2e3c56;
+  }
+  .user {
+    margin-left: auto;
+    padding-right: 1.25em;
+  }
+</style>
+
+<nav>
+  <ul>
+    <li class='logo'>
+      <a href='.'>home</a>
+    </li>
+    <li class='user'>
+      <a rel=prefetch href='blog'>user</a>
+    </li>
+  </ul>
+</nav>
